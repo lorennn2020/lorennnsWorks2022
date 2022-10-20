50e3bd7634fafe5fb0b94ed76a9f92d460e2d61a
lorennn2020 <roy830817@yahoo.com.tw>
2022-10-20 23:53:13 +0800

    2022.10.20 2353

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,15 @@
 function init() {
+    ////// index //////
     $("#headerAbout").click(function () {
         $("body").toggleClass("openAbout");
     });
     $("#headerWork").click(function () {
         $("body").removeClass("openAbout");
+    });
+
+    ////// inner //////
+    $("article#motionGrapicInner .inner_video").mouseenter(function () {
+        $(this).children("video").get(0).play();
     });
 }
 jQuery(document).ready(init());

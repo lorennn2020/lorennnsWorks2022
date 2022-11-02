6c41c616790b1b36140d986c6f2d0abcffb779b1
lorennn2020 <roy830817@yahoo.com.tw>
2022-11-02 10:12:08 +0800

    2022.11.2 1011

--- javascript/common.js
+++ javascript/common.js
@@ -19,12 +19,30 @@ var $desertPos = 0;
 var $toHome, $toSea, $toDesert;
 var $taking, $clicking, $bag_spaceLR, $mousePosX, $mousePosY, $ingredientsBlock;
   
+// preload images
+var $preloadImages = new Array();
+function preloadImg() {  
+    for (let i = 0; i < preloadImg.arguments.length; i++) {
+        $preloadImages[i] = new Image();
+        $preloadImages[i].src = preload.arguments[i];
+    }  
+}  
+preloadImg(  
+    "https://lorennn2020.github.io/lorennnsWorks2022/img/scene-beginning_900x600.gif",  
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/motionGrapic.png",
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/ROOM.png",
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/BENNS.png",
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/chinesePainting_cover.png",
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/3D_anchor_cover.png",
+    "https://lorennn2020.github.io/lorennnsWorks2022/worksImg/3Dcamping_cover.png"
+)  
+
 // 判別Firefox
 if (navigator.userAgent.indexOf('Firefox') !== -1) {
     $('body').addClass('isFireFox');
 }
 
-// Safari
+// 判別Safari
 if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") < 0) {
     $('body').addClass('isSafari');
 }
